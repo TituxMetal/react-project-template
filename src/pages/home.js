e673a8ead18b2aced99cd1d3e43b7f45259ce3ad
Titux Metal <github@lgdweb.fr>
2020-07-29 03:04:39 +0200

    Adding home.js file in pages; component for home page

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { MainPage } from '~/components'
+
+const Home = () => (
+  <MainPage.Splash>
+    <MainPage.Title>Home Page</MainPage.Title>
+  </MainPage.Splash>
+)
+
+export default Home
