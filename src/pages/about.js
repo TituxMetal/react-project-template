3ee927392da3d1d90113b3303cb1dfd789610585
Titux Metal <github@lgdweb.fr>
2020-09-22 10:44:29 +0200

    Updating about.js file in pages; using common styled components in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
-import { MainPage } from '~/components'
+import { Container, Text, Title } from '~/components'
 
-const About = () => <MainPage.Title>About Page</MainPage.Title>
+const About = () => (
+  <Container>
+    <Title>About Page</Title>
+    <Text>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid,
+      explicabo quasi repellendus quibusdam atque accusantium. Accusamus eius
+      impedit inventore magnam? Vero ut odit repellat ex doloremque corporis
+      maiores. Laboriosam, itaque.
+    </Text>
+  </Container>
+)
 
 export default About
