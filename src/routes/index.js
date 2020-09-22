5389196fdec503d5370d7193d8887c4ef5c2693e
Titux Metal <github@lgdweb.fr>
2020-09-22 10:42:07 +0200

    Updating index.js file in routes; wrapping all routes in the layout component

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
-import React, { lazy, Suspense} from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import React, { lazy, Suspense } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import { FooterContainer } from '~/containers'
+import { Layout } from '~/core'
 
 const About = lazy(() => import('~/pages/about'))
 const Home = lazy(() => import('~/pages/home'))
@@ -10,11 +10,10 @@ const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Suspense fallback={<p>Loading Page...</p>}>
-        <>
-        <Route exact path='/' component={Home} />
-        <Route path='/about' component={About} />
-        <FooterContainer />
-        </>
+        <Layout>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+        </Layout>
       </Suspense>
     </Switch>
   </BrowserRouter>
