fa1dd54b0545d9061da2af7d8852338ff307d8c5
Titux Metal <github@lgdweb.fr>
2020-09-22 10:31:26 +0200

    Adding styled components in core/layout

---
+++ src/core/layout/styled/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Main = styled.div`
+  flex: 1 0 auto;
+  margin: auto;
+  padding: 0 1rem;
+  width: 100%;
+`
+
+export const Wrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`
